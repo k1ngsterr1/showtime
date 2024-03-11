15532d85ef84d6dfaabb6d375b7420555531ce46
Chechen Daddy <ruslanmakhmatov@gmail.com>
2024-03-11 13:50:57 +0600

    Version 1.1.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()],
+  integrations: [react(), tailwind(), icon(), mdx()]
 });
