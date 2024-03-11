30937bc5b07374fd3ff8b40df1e1cc5771f196e8
senseiGai <swerss564@gmail.com>
2024-03-11 22:53:22 +0600

    version 1.2.8

--- src/features/Gallery/index.tsx
+++ src/features/Gallery/index.tsx
@@ -1,27 +1,55 @@
+import React from "react";
+import { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
+import RevolverButton from "@shared/ui/Buttons/RevolverButton/index.astro";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-import men from "../../assets/Gallery/men.webp";
-import womanman from "../../assets/Gallery/womanman.webp";
-import women from "../../assets/Gallery/women.webp";
-import womenjar from "../../assets/Gallery/womenjar.webp";
-import womenkatana from "../../assets/Gallery/womenkatana.webp";
+import styles from "./styles.module.scss";
 
-const photos = [men, womanman, women, womenjar, womenkatana];
+const GalleryCustom = ({ photos }) => {
+  const swiperRef = React.useRef<Swiper | null>(null);
 
-const GalleryCustom = () => {
+  const handlePrev = () => {
+    if (swiperRef.current) {
+      swiperRef.current.slidePrev();
+    }
+  };
+
+  const handleNext = () => {
+    if (swiperRef.current) {
+      swiperRef.current.slideNext();
+    }
+  };
+
   return (
     <div className="container">
+      <div
+      // className="pc-gallery"
+      // style={{ marginBottom: "clamp(64px,6.66624vw,256px)" }}
+      >
+        <div className="gallery-nav">
+          <RevolverButton
+            buttonType="gallery"
+            className="previous"
+            onClick={handlePrev}
+          ></RevolverButton>
+          <RevolverButton
+            buttonType="gallery"
+            className="next"
+            onClick={handleNext}
+          ></RevolverButton>
+        </div>
+      </div>
       <Swiper
-        classNams={styles}
+        className={styles.swiper}
         modules={[Navigation]}
         spaceBetween={50}
-        slidesPerView={3}
+        slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
