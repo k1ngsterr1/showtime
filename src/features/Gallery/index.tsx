23f6343b1f42f6a647c49c2523667bd3dc42bfee
senseiGai <swerss564@gmail.com>
2024-03-11 20:34:17 +0600

    version 1.2.7

--- src/features/Gallery/index.tsx
+++ src/features/Gallery/index.tsx
@@ -1,5 +1,41 @@
-import styles from "./styles.module.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 
-import BigText from "./styles.module.scss";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
-import Paragraph from "./styles.module.scss";
+import men from "../../assets/Gallery/men.webp";
+import womanman from "../../assets/Gallery/womanman.webp";
+import women from "../../assets/Gallery/women.webp";
+import womenjar from "../../assets/Gallery/womenjar.webp";
+import womenkatana from "../../assets/Gallery/womenkatana.webp";
+
+const photos = [men, womanman, women, womenjar, womenkatana];
+
+const GalleryCustom = () => {
+  return (
+    <div className="container">
+      <Swiper
+        classNams={styles}
+        modules={[Navigation]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        {photos.map((photo, index) => (
+          <SwiperSlide key={index}>
+            <img src={photo.src} alt={`Slide ${index + 1}`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default GalleryCustom;
