4418ce0da87e84574b18fbd2a8a8f1e22f1e10e3
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-03-12 20:54:09 +0600

    Version 1.4.2

--- src/features/Header/index.tsx
+++ src/features/Header/index.tsx
@@ -12,15 +12,15 @@ export const Header = () => {
       <div className={styles.header__content}>
         <Logo />
         <nav className={styles.header__content__links}>
-          <LinkText text="Главная" to="main" />
-          <LinkText text="О нас" to="about" />
-          <LinkText text="Услуги" to="services" />
-          <LinkText text="Расписание" to="timetable" />
-          <LinkText text="Правила" to="rules" />
-          <LinkText text="Рейтинг" to="rating" />
-          <LinkText text="Контакты" to="contacts" />
+          <LinkText text="Главная" to="main" href="" />
+          <LinkText text="О нас" to="about" href="" />
+          <LinkText text="Услуги" to="services" href="" />
+          <LinkText text="Расписание" to="timetable" href="" />
+          <LinkText text="Правила" to="rules" href="" />
+          <LinkText text="Рейтинг" to="rating" href="" />
+          <LinkText text="Контакты" to="contacts" href="" />
         </nav>
-        <div className="flex items-center justify-center gap-8">
+        <div className="flex items-center justify-center gap-8 overflow-hidden">
           <LanguageButton />
           <MenuButton />
         </div>
