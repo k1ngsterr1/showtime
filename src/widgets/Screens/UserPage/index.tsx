6f269baaaedd425d7345a15467bc28056b79ddd1
Chechen Daddy <ruslanmakhmatov@gmail.com>
2024-03-14 03:48:54 +0600

    Version --USER_PAGE_ADD

--- src/widgets/Screens/UserPage/index.tsx
+++ src/widgets/Screens/UserPage/index.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import bgImage from "@assets/Main/mafia_shadow.webp";
+
+import styles from "./styles.module.scss";
 
 export const UserScreen = () => {
-  return <div>UserP</div>;
+  return (
+    <main className={styles.user_screen}>
+      <div className={styles.user_screen__container}></div>
+      <div className={styles.user_screen__upper}></div>
+      <img
+        src={bgImage.src}
+        className={styles.user_screen__bg}
+        alt="mafia_bg_image"
+      />
+    </main>
+  );
 };
