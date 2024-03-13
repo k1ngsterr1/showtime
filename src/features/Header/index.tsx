f634b4d3992c0be171df40c7f3c20990e7d693ba
Chechen Daddy <ruslanmakhmatov@gmail.com>
2024-03-13 21:51:19 +0600

    Version MOYA MAMA LUBIT MENYA

--- src/features/Header/index.tsx
+++ src/features/Header/index.tsx
@@ -1,4 +1,3 @@
-import Link from "react-scroll";
 import Logo from "@shared/ui/Icons/Logo";
 import { MenuButton } from "@shared/ui/Icons/MenuButton";
 import { LanguageButton } from "@shared/ui/LanguageButton";
@@ -10,25 +9,35 @@ import { useCustomMenu } from "@shared/lib/hooks/useCustomMenu";
 export const Header = () => {
   const { onOpen } = useCustomMenu();
   return (
-    <header className={styles.header}>
-      <div className={styles.header__content}>
-        <Logo />
-        <nav className={styles.header__content__links}>
-          <LinkText text="Главная" to="main" href="" />
-          <LinkText text="О нас" to="about" href="" />
-          <LinkText text="Услуги" to="services" href="" />
-          <LinkText text="Расписание" to="timetable" href="" />
-          <LinkText text="Правила" to="rules" href="" />
-          <LinkText text="Рейтинг" to="rating" href="" />
-          <LinkText text="Контакты" to="contacts" href="" />
-        </nav>
-        <div className="flex items-center justify-center gap-8 overflow-hidden">
-          <LanguageButton />
+    <>
+      <header className={styles.header}>
+        <div className={styles.header__content}>
+          <Logo />
+          <nav className={styles.header__content__links}>
+            <LinkText text="Главная" to="main" href="" />
+            <LinkText text="О нас" to="about" href="" />
+            <LinkText text="Услуги" to="services" href="" />
+            <LinkText text="Расписание" to="timetable" href="" />
+            <LinkText text="Правила" to="rules" href="" />
+            <LinkText text="Рейтинг" to="rating" href="" />
+            <LinkText text="Контакты" to="contacts" href="" />
+          </nav>
+          <div className="flex items-center justify-center gap-8 overflow-hidden">
+            <LanguageButton />
+            <span className="overflow-hidden" onClick={onOpen}>
+              <MenuButton />
+            </span>
+          </div>
+        </div>
+      </header>
+      <header className={styles.header_mob}>
+        <div className="flex items-center justify-between w-full mt-8">
+          <Logo />
           <span className="overflow-hidden" onClick={onOpen}>
             <MenuButton />
           </span>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
--- src/widgets/Screens/ContactForm/index.tsx
+++ src/widgets/Screens/ContactForm/index.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Input } from "@shared/ui/Inputs/DefaultInput";
 import { ReactButton } from "@shared/ui/Buttons/DefaultReactButton";
 import { Selector } from "@shared/ui/Selector";
-import Button from "@shared/ui/Buttons/DefaultButton/index.astro";
 import GoogleMap from "@shared/ui/GoogleMap/index";
 import styles from "./styles.module.scss";
 
@@ -10,65 +9,147 @@ export const Form = () => {
   const [selectedRole, setSelectedRole] = useState<string>("");
 
   return (
-    <section className={styles.form_screen} id="contacts">
-      <div className={styles.form_screen__container}>
-        <h6 className="text-primary-red m-auto">Контакты</h6>
-        <div className="flex items-center m-auto w-[60%] justify-between">
-          <ReactButton text="Отправить" buttonType="filled" margin="mt-8" />
-          <ReactButton text="Заказать" buttonType="transparent" margin="mt-8" />
-          <ReactButton text="Запись" buttonType="transparent" margin="mt-8" />
-          <ReactButton
-            text="Задать вопрос"
-            buttonType="transparent"
-            margin="mt-8"
-          />
-        </div>
-        <form className={styles.form_screen__container__form}>
-          <div className="flex justify-between items-start">
-            <div className="flex flex-col items-start">
-              <Input
-                type="text"
-                inputType="default-red"
-                placeholder="Ваше имя"
-                margin="mt-8"
-              />
-              <Input
-                type="phone"
-                inputType="default-red"
-                placeholder="Номер телефона"
-                margin="mt-8"
-              />
-              <Selector
-                placeholder="Продукт"
-                selectedValue={selectedRole}
-                onChange={setSelectedRole}
-                items={[
-                  "Вечерняя Игра",
-                  "Вечерняя Игра",
-                  "Вечерняя Игра",
-                  "Вечерняя Игра",
-                ]}
-              />
-              <ReactButton text="Отправить" buttonType="filled" margin="mt-8" />
+    <>
+      <section className={styles.form_screen} id="contacts">
+        <div className={styles.form_screen__container}>
+          <h6 className="text-primary-red m-auto">Контакты</h6>
+          <div className="flex items-center m-auto w-[60%] justify-between">
+            <ReactButton text="Отправить" buttonType="filled" margin="mt-8" />
+            <ReactButton
+              text="Заказать"
+              buttonType="transparent"
+              margin="mt-8"
+            />
+            <ReactButton text="Запись" buttonType="transparent" margin="mt-8" />
+            <ReactButton
+              text="Задать вопрос"
+              buttonType="transparent"
+              margin="mt-8"
+            />
+          </div>
+          <form className={styles.form_screen__container__form}>
+            <div className="flex justify-between items-start">
+              <div
+                className={`${"flex flex-col items-start"} ${styles.buttons}`}
+              >
+                <Input
+                  type="text"
+                  inputType="default-red"
+                  placeholder="Ваше имя"
+                  margin="mt-8"
+                />
+                <Input
+                  type="phone"
+                  inputType="default-red"
+                  placeholder="Номер телефона"
+                  margin="mt-8"
+                />
+                <Selector
+                  placeholder="Продукт"
+                  selectedValue={selectedRole}
+                  onChange={setSelectedRole}
+                  items={[
+                    "Вечерняя Игра",
+                    "Вечерняя Игра",
+                    "Вечерняя Игра",
+                    "Вечерняя Игра",
+                  ]}
+                />
+                <ReactButton
+                  text="Отправить"
+                  buttonType="filled"
+                  margin="mt-8"
+                />
+              </div>
+              <GoogleMap />
             </div>
-            <GoogleMap />
+            <div className="flex flex-col">
+              <a
+                href="mailto:showtime@mafia.kz"
+                className={`${styles.form_screen__container__form__link} mt-5`}
+              >
+                showtime@mafia.kz
+              </a>
+              <a
+                href="tel:+77003333826"
+                className={`${styles.form_screen__container__form__link} mt-5`}
+              >
+                +7 (700) 333-38-26
+              </a>
+            </div>
+          </form>
+        </div>
+      </section>
+      <section className={styles.form_screen_mob} id="contacts">
+        <div className={styles.form_screen__container}>
+          <h6 className="text-primary-red m-auto">Контакты</h6>
+          <div className="flex items-center m-auto flex-col">
+            <ReactButton text="Отправить" buttonType="filled" margin="mt-8" />
+            <ReactButton
+              text="Заказать"
+              buttonType="transparent"
+              margin="mt-8"
+            />
+            <ReactButton text="Запись" buttonType="transparent" margin="mt-8" />
+            <ReactButton
+              text="Задать вопрос"
+              buttonType="transparent"
+              margin="mt-8"
+            />
           </div>
-          <div className="flex flex-col">
-            <a
-              href="mailto:showtime@mafia.kz"
-              className={`${styles.form_screen__container__form__link} mt-5`}
-            >
-              showtime@mafia.kz
-            </a>
-            <a
-              href="tel:+77003333826"
-              className={`${styles.form_screen__container__form__link} mt-5`}
-            >
-              +7 (700) 333-38-26
-            </a>
-          </div>
-        </form>
-      </div>
-    </section>
+          <form className={styles.form_screen__container__form}>
+            <div className="flex justify-between items-start">
+              <div
+                className={`${"flex flex-col items-center"} ${styles.buttons}`}
+              >
+                <Input
+                  type="text"
+                  inputType="default-red"
+                  placeholder="Ваше имя"
+                  margin="mt-8"
+                />
+                <Input
+                  type="phone"
+                  inputType="default-red"
+                  placeholder="Номер телефона"
+                  margin="mt-8"
+                />
+                <Selector
+                  placeholder="Продукт"
+                  selectedValue={selectedRole}
+                  onChange={setSelectedRole}
+                  items={[
+                    "Вечерняя Игра",
+                    "Вечерняя Игра",
+                    "Вечерняя Игра",
+                    "Вечерняя Игра",
+                  ]}
+                />
+                <ReactButton
+                  text="Отправить"
+                  buttonType="filled"
+                  margin="mt-8"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <a
+                href="mailto:showtime@mafia.kz"
+                className={`${styles.form_screen__container__form__link} mt-5`}
+              >
+                showtime@mafia.kz
+              </a>
+              <a
+                href="tel:+77003333826"
+                className={`${styles.form_screen__container__form__link} mt-5`}
+              >
+                +7 (700) 333-38-26
+              </a>
+            </div>
+          </form>
+          <GoogleMap />
+        </div>
+      </section>
+    </>
   );
 };
