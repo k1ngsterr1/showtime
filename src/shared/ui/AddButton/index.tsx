266e903093b9f35af9ed13e1599e9017b2c27b81
senseiGai <swerss564@gmail.com>
2024-03-16 02:36:47 +0600

    version --ADMIN_4

---
+++ src/shared/ui/AddButton/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+
+import styles from "./styles.module.scss";
+
+interface ButtonProps {
+  buttonType: "filled" | "outline";
+  text: string;
+  margin?: string;
+  type?: string;
+}
+
+const AddButton: React.FC<ButtonProps> = ({ buttonType, text, margin }) => {
+  const buttonClass = `${styles.button} ${styles[buttonType]} ${margin ? margin : ""}`;
+
+  return (
+    <button className={buttonClass}>
+      <FontAwesomeIcon className={styles.buttonIcon} icon={faCirclePlus} />
+      <span>{text}</span>
+    </button>
+  );
+};
+
+export default AddButton;
