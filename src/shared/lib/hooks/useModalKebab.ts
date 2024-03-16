b59145dc0e0a723e345acb8d219bb49c22636f30
Chechen Daddy <ruslanmakhmatov@gmail.com>
2024-03-16 18:38:40 +0600

    Version --SHOPPER

--- src/shared/lib/hooks/useModalKebab.ts
+++ src/shared/lib/hooks/useModalKebab.ts
@@ -1,27 +1,27 @@
-import React from "react";
-import styles from "./Modal.module.scss";
+// import React from "react";
+// import styles from "./Modal.module.scss";
 
-interface ModalProps {
-  isOpen: boolean;
-  closeModal: () => void;
-}
+// interface ModalProps {
+//   isOpen: boolean;
+//   closeModal: () => void;
+// }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, closeModal, children }) => {
-  const handleBackdropClick = () => {
-    closeModal();
-  };
+// const Modal: React.FC<ModalProps> = ({ isOpen, closeModal, children }) => {
+//   const handleBackdropClick = () => {
+//     closeModal();
+//   };
 
-  return (
-    <>
-      {isOpen && (
-        <div className={styles.modalBackdrop} onClick={handleBackdropClick}>
-          <div className={styles.modalContent}>
-            {children}
-          </div>
-        </div>
-      )}
-    </>
-  );
-};
+//   return (
+//     <>
+//       {isOpen && (
+//         <div className={styles.modalBackdrop} onClick={handleBackdropClick}>
+//           <div className={styles.modalContent}>
+//             {children}
+//           </div>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
 
-export default Modal;
+// export default Modal;
