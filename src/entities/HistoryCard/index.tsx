7ff7d52ab1c31d743bc4a2bff075b756677666c4
fwefew123 <artem.andreev200012@mail.ru>
2024-03-17 12:55:36 +0500

    Version FINAL ANAL

--- src/entities/HistoryCard/index.tsx
+++ src/entities/HistoryCard/index.tsx
@@ -9,18 +9,14 @@ interface Props {
 
 const ReviewCard: React.FC<Props> = ({ data, name, paragraph }) => (
   <div className={styles.review_card}>
-    <div className="flex flex-col items-start p-4">
-      <div className="flex gap-6 pt-2">
-        <span>{name}</span>
-      </div>
-      <span className="font-killbill">{data}</span>
-      <div className="pt-4">
-        <p
-          className={`${"mt-4 text-primary-dark text-lg"} ${styles.paragraph}`}
-        >
-          {paragraph}
-        </p>
-      </div>
+    <div className="flex gap-6 pt-2">
+      <span className={styles.name}>{name}</span>
+    </div>
+    <span className={styles.data}>{data}</span>
+    <div className="pt-4">
+      <p className={`${"mt-4 text-primary-dark text-lg"} ${styles.paragraph}`}>
+        {paragraph}
+      </p>
     </div>
   </div>
 );
