f9a50d097070ab59fd05444062c9bd33d0a52872
senseiGai <swerss564@gmail.com>
2024-03-19 01:51:10 +0600

    version --ADMIN_SHESHEN_AMIN

---
+++ src/widgets/Screens/AdminPage/Team/index.tsx
@@ -0,0 +1,36 @@
+import { AdminPanel } from '@features/AdminFeatures/AdminPanel/index'
+import { adminpanel } from '@features/AdminFeatures/AdminPanel/index'
+import AddButton from '../../../../shared/ui/AddButton/index'
+import AdminHeader from '@features/AdminFeatures/AdminHeader/index'
+import AdminTeam from '@features/AdminFeatures/AdminTeam/index'
+import { users } from '@features/AdminFeatures/AdminTeam/index'
+
+import '@shared/styles/global.scss'
+
+import styles from './styles.module.scss'
+
+import Oleg from '@assets/Admin/oleg.webp'
+
+export const Team = () => {
+	return (
+		<main className={styles.client}>
+			<div className={styles.client__left}>
+				<AdminPanel adminpanel={adminpanel} />
+			</div>
+			<div className={styles.client__main}>
+				<AdminHeader name="Bafomet Nurmukhamed" position="God" photo={Oleg} />
+				<div className={styles.client__main__functional}>
+					<div className={styles.client__main__functional__header}>
+						<h1 className={styles.client__main__functional__header_heading}>
+							Управление пользователями
+						</h1>
+						<AddButton buttonType="filled" text="Добавить" />
+					</div>
+					<div className={styles.client__main__functional__teammates}>
+						<AdminTeam users={users} />
+					</div>
+				</div>
+			</div>
+		</main>
+	)
+}
