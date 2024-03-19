d4ce06e917aeb3f4911beecd8b3bd49e838a2d4c
fwefew123 <artem.andreev200012@mail.ru>
2024-03-19 13:25:34 +0500

    Version Cards

--- src/features/Swiper_Components/CardGallery/index.tsx
+++ src/features/Swiper_Components/CardGallery/index.tsx
@@ -18,41 +18,46 @@ interface ICardGalleryProps {
 
 export const CardGallery: React.FC<ICardGalleryProps> = ({ cards }) => {
 	const swiperRef = React.useRef<Swiper | null>(null)
+	const [activeCardIndex, setActiveCardIndex] = React.useState(0);
+
+
 
 	return (
 		<>
-			<Swiper
-				className={styles.swiper}
-				modules={[EffectCards]}
-				effect={'cards'}
-				onSwiper={(swiperInstance) => {
-					swiperRef.current = swiperInstance
-				}}
-			>
-				{cards.map((card, index) => (
-					<SwiperSlide key={index} className="overflow-hidden">
-						<div className="flex items-center justify-center">
-							<Card
-								name={card.name}
-								paragraph={card.paragraph}
-								iconType={card.iconType.src}
-								icon={card.icon.src}
-							/>
-						</div>
-					</SwiperSlide>
-				))}
-			</Swiper>
-			<div className={styles.text_container}>
-				<div className="flex items-center gap-8">
-					<span className={styles.text_container__heading}>Заголовок</span>
-					<SelectedBar text="Тип игрока" />
+			<div className="mt-16">
+				<Swiper
+					className={styles.swiper}
+					modules={[EffectCards]}
+					effect={'cards'}
+					onSwiper={(swiperInstance) => {
+						swiperRef.current = swiperInstance;
+					}}
+					onSlideChange={(swiper) => {
+						setActiveCardIndex(swiper.activeIndex);
+					}}
+				>
+					{cards.map((card, index) => (
+						<SwiperSlide key={index} className="overflow-hidden">
+							<div className="flex items-center justify-center">
+								<Card
+									name={card.name}
+									paragraph={card.paragraph}
+									iconType={card.iconType.src}
+									icon={card.icon.src}
+								/>
+							</div>
+						</SwiperSlide>
+					))}
+				</Swiper>
+				<div className={styles.text_container}>
+					<div className="flex items-center gap-8">
+						<span className={styles.text_container__heading}>{cards[activeCardIndex].name}</span>
+						<SelectedBar text="Тип игрока" />
+					</div>
+					<p className={`${styles.text_container__paragraph} mt-4 w-[40%]`}>
+						{cards[activeCardIndex].paragraph}
+					</p>
 				</div>
-				<p className={`${styles.text_container__paragraph} mt-4 w-[40%]`}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua. <br /> <br />
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-					ut labore et dolore magna aliqua.
-				</p>
 			</div>
 		</>
 	)
