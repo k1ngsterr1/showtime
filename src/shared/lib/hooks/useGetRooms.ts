2adf94c30b8043026955df1a9687486473308d03
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-03-21 23:27:44 +0600

    Version --SOCKETS

--- src/shared/lib/hooks/useGetRooms.ts
+++ src/shared/lib/hooks/useGetRooms.ts
@@ -1,9 +1,11 @@
-// useGetRooms.js
 import axios from 'axios'
+import io from 'socket.io-client'
 import { useState, useEffect } from 'react'
 
 export function useGetRooms() {
 	const [rooms, setRooms] = useState([])
+
+	const socket = io('http://localhost:4000', { path: '/sockets/' })
 
 	useEffect(() => {
 		let isMounted = true
@@ -12,7 +14,7 @@ export function useGetRooms() {
 			try {
 				const response = await axios.get('https://showtime.up.railway.app/api/rooms/get-rooms')
 				if (isMounted) {
-					setRooms(response.data.rooms) // Adjust according to actual response structure
+					setRooms(response.data.rooms)
 				}
 			} catch (error) {
 				console.error('There was an error fetching the rooms:', error)
@@ -21,11 +23,25 @@ export function useGetRooms() {
 
 		fetchRooms()
 
-		const intervalId = setInterval(fetchRooms, 30000)
+		socket.on('connect', () => {
+			console.log('Connected to socket server')
+		})
+
+		socket.on('roomCreated', (newRoom) => {
+			console.log('ROOM CREATED!!!')
+			setRooms((prevRooms) => [...prevRooms, newRoom])
+		})
+
+		socket.on('roomUpdated', (updatedRoom) => {
+			setRooms((prevRooms) =>
+				prevRooms.map((room) => (room.id === updatedRoom.id ? updatedRoom : room))
+			)
+		})
 
 		return () => {
-			isMounted = false
-			clearInterval(intervalId)
+			socket.off('connect')
+			socket.off('roomCreated')
+			socket.off('roomUpdated')
 		}
 	}, [])
 
