105ed085964c3d58b9e76b39f8d64af88612577b
senseiGai <swerss564@gmail.com>
2024-03-31 21:33:01 +0500

    REFACTORING

---
+++ src/shared/lib/content/aboutContent.ts
@@ -0,0 +1,26 @@
+export const cardData = [
+	{
+		bigText: 'Lorem Ipsum',
+		cardType: 'dark',
+		paragraph:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		bigText: 'Lorem Ipsum',
+		cardType: 'dark',
+		paragraph:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		bigText: 'Lorem Ipsum',
+		cardType: 'dark',
+		paragraph:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		bigText: 'Lorem Ipsum',
+		cardType: 'dark',
+		paragraph:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	}
+]
---
+++ src/shared/lib/content/servicesMainContent.ts
@@ -0,0 +1,27 @@
+import man_01 from '@assets/Main/man_01.png'
+import man_02 from '@assets/Main/man_02.png'
+import man_03 from '@assets/Main/man_03.png'
+
+export const cardsData = [
+	{
+		image: man_01.src,
+		number: '01',
+		heading: 'Городская мафия',
+		paragraph:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		image: man_02.src,
+		number: '02',
+		heading: 'Классическая мафия',
+		paragraph:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	},
+	{
+		image: man_03.src,
+		number: '03',
+		heading: 'Бункер мафия',
+		paragraph:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	}
+]
