08a32945e987f0753b5028c0d14deb19211afad0
senseiGai <swerss564@gmail.com>
2024-04-02 16:03:04 +0500

    RATING_FIXED

--- src/entities/Tab_Components/RatingTab/index.tsx
+++ src/entities/Tab_Components/RatingTab/index.tsx
@@ -1,50 +1,39 @@
 import { RatingHeader } from '@shared/ui/RatingHeader'
-
 import styles from './styles.module.scss'
 
-import Cub from '@assets/Main/cub.svg'
+interface RatingItem {
+	kfc: string
+	wins: string
+	icon: string
+	margin: string
+	name: string
+	number: string
+	games: string
+}
 
-export const RatingTab = () => {
+interface RatingTabProps {
+	rating: RatingItem[]
+}
+
+const RatingTab: React.FC<RatingTabProps> = ({ rating }) => {
 	return (
 		<div className={styles.container}>
 			<div className="">
-				<RatingHeader
-					kfc="0.3"
-					wins="52"
-					icon={Cub.src}
-					margin="mt-8"
-					name="Ruslan Islam"
-					number="1"
-					games="100"
-				/>
-				<RatingHeader
-					kfc="0.4"
-					wins="52"
-					icon={Cub.src}
-					margin="mt-4"
-					name="Baida Baidarka"
-					number="2"
-					games="100"
-				/>
-				<RatingHeader
-					kfc="0.9"
-					wins="52"
-					icon={Cub.src}
-					margin="mt-4"
-					name="Artyom Billy"
-					number="3"
-					games="100"
-				/>
-				<RatingHeader
-					kfc="0.6"
-					wins="52"
-					icon={Cub.src}
-					margin="mt-4"
-					name="Goidaaaaaa"
-					number="4"
-					games="100"
-				/>
+				{rating.map((item, index) => (
+					<RatingHeader
+						key={index}
+						kfc={item.kfc}
+						wins={item.wins}
+						icon={item.icon}
+						margin={item.margin}
+						name={item.name}
+						number={item.number}
+						games={item.games}
+					/>
+				))}
 			</div>
 		</div>
 	)
 }
+
+export default RatingTab
---
+++ src/shared/lib/content/ratingContent.ts
@@ -0,0 +1,49 @@
+import Cub from '@assets/Main/cub.svg'
+
+export const rating = [
+	{
+		kfc: '0.3',
+		wins: '52',
+		icon: Cub.src,
+		margin: 'mt-8',
+		name: 'Ruslan Islam',
+		number: '1',
+		games: '100'
+	},
+	{
+		kfc: '0.3',
+		wins: '52',
+		icon: Cub.src,
+		margin: 'mt-8',
+		name: 'Ruslan Islam',
+		number: '1',
+		games: '100'
+	},
+	{
+		kfc: '0.3',
+		wins: '52',
+		icon: Cub.src,
+		margin: 'mt-8',
+		name: 'Ruslan Islam',
+		number: '1',
+		games: '100'
+	},
+	{
+		kfc: '0.3',
+		wins: '52',
+		icon: Cub.src,
+		margin: 'mt-8',
+		name: 'Ruslan Islam',
+		number: '1',
+		games: '100'
+	},
+	{
+		kfc: '0.3',
+		wins: '52',
+		icon: Cub.src,
+		margin: 'mt-8',
+		name: 'Ruslan Islam',
+		number: '1',
+		games: '100'
+	}
+]
