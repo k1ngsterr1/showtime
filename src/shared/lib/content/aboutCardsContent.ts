5605a8c127413cc97450160221857cac81a70abe
senseiGai <swerss564@gmail.com>
2024-04-06 12:58:45 +0500

    BUGS_FIXED_CONTENT_ADDED

---
+++ src/shared/lib/content/aboutCardsContent.ts
@@ -0,0 +1,26 @@
+export const aboutCardsData = [
+	{
+		bigText: 'Lorem Ipsum',
+		paragraph:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		cardType: 'white'
+	},
+	{
+		bigText: 'Lorem Ipsum',
+		paragraph:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		cardType: 'white'
+	},
+	{
+		bigText: 'Lorem Ipsum',
+		paragraph:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		cardType: 'white'
+	},
+	{
+		bigText: 'Lorem Ipsum',
+		paragraph:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		cardType: 'white'
+	}
+]
