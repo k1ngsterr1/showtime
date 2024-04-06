83d85de3384d6f86f5944196207d34556b8109a2
fwefew123 <artem.andreev200012@mail.ru>
2024-04-06 21:12:16 +0500

    Version Check

--- src/widgets/Screens/SelectedRules/index.tsx
+++ src/widgets/Screens/SelectedRules/index.tsx
@@ -29,48 +29,48 @@ export const RulesContent: React.FC<IFormContentProps> = ({ rulesType, cards }) 
 						<Paragraph text="dfgimdfugsdo[fgmsdfiogsdfgjsdofg" paragraphType={'white'} />
 					</div>
 				)
-			case 'roles':
-				return (
-					<div>
-						<div className="mt-16">
-							<Swiper
-								className={styles.swiper}
-								modules={[EffectCards]}
-								effect={'cards'}
-								onSwiper={(swiperInstance) => {
-									swiperRef.current = swiperInstance
-								}}
-								onSlideChange={(swiper) => {
-									setActiveCardIndex(swiper.activeIndex)
-								}}
-							>
-								{cards.map((card, index) => (
-									<SwiperSlide key={index} className="overflow-hidden">
-										<div className="flex items-center justify-center">
-											<Card
-												name={card.name}
-												paragraph={card.paragraph}
-												iconType={card.iconType.src}
-												icon={card.icon.src}
-											/>
-										</div>
-									</SwiperSlide>
-								))}
-							</Swiper>
-							<div className={styles.text_container}>
-								<div className="flex items-center gap-8">
-									<span className={styles.text_container__heading}>
-										{cards[activeCardIndex].name}
-									</span>
-									<SelectedBar text="Тип игрока" />
-								</div>
-								<p className={`${styles.text_container__paragraph} mt-4 w-[40%]`}>
-									{cards[activeCardIndex].paragraph}
-								</p>
-							</div>
-						</div>
-					</div>
-				)
+			// case 'roles':
+			// 	return (
+			// 		<div>
+			// 			<div className="mt-16">
+			// 				<Swiper
+			// 					className={styles.swiper}
+			// 					modules={[EffectCards]}
+			// 					effect={'cards'}
+			// 					onSwiper={(swiperInstance) => {
+			// 						swiperRef.current = swiperInstance
+			// 					}}
+			// 					onSlideChange={(swiper) => {
+			// 						setActiveCardIndex(swiper.activeIndex)
+			// 					}}
+			// 				>
+			// 					{cards.map((card, index) => (
+			// 						<SwiperSlide key={index} className="overflow-hidden">
+			// 							<div className="flex items-center justify-center">
+			// 								<Card
+			// 									name={card.name}
+			// 									paragraph={card.paragraph}
+			// 									iconType={card.iconType.src}
+			// 									icon={card.icon.src}
+			// 								/>
+			// 							</div>
+			// 						</SwiperSlide>
+			// 					))}
+			// 				</Swiper>
+			// 				<div className={styles.text_container}>
+			// 					<div className="flex items-center gap-8">
+			// 						<span className={styles.text_container__heading}>
+			// 							{cards[activeCardIndex].name}
+			// 						</span>
+			// 						<SelectedBar text="Тип игрока" />
+			// 					</div>
+			// 					<p className={`${styles.text_container__paragraph} mt-4 w-[40%]`}>
+			// 						{cards[activeCardIndex].paragraph}
+			// 					</p>
+			// 				</div>
+			// 			</div>
+			// 		</div>
+			// 	)
 			case 'cardmob':
 				return <div></div>
 			case 'rulesmob':
