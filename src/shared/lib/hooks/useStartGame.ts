a7f4059382072c1292365f5f7780079b677330b9
senseiGai <swerss564@gmail.com>
2024-04-07 14:02:55 +0500

    TEST --STARTGAME

--- src/shared/lib/hooks/useStartGame.ts
+++ src/shared/lib/hooks/useStartGame.ts
@@ -4,7 +4,16 @@ import { useState, useEffect } from 'react'
 export function useStartGame() {
 	const startGame = async (roomId: number) => {
 		try {
-			const response = await axios.post(`http://localhost:4000/api/game/start-game/${roomId}`)
+			const response = await axios.post(
+				`http://localhost:4200/api/game/start-game/${roomId}`,
+				null,
+				{
+					withCredentials: true,
+					headers: {
+						'Content-Type': 'multipart/form-data'
+					}
+				}
+			)
 			console.log(response.data)
 		} catch (error) {
 			console.error(error)
