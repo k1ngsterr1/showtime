6ccaaa25875d96c3d297b9676cc43cc091fe34b9
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-04-13 00:42:21 +0600

    --CHANGE RANKS

--- src/features/AdminFeatures/AdminRanks/index.tsx
+++ src/features/AdminFeatures/AdminRanks/index.tsx
@@ -12,48 +12,52 @@ import { useChangeRank } from '@shared/lib/hooks/useChangeRank'
 
 const AdminRanks: React.FC = () => {
 	const [email, setEmail] = useState<string>()
-	const [rank, setRank] = useState<string>()
+	const [newRank, setRank] = useState<string>()
 	const { changeRank } = useChangeRank()
 
 	const handleSubmit = async (event: React.FormEvent) => {
 		event.preventDefault()
 
-		if (email && rank !== null) {
+		if (email && newRank !== null) {
 			const formData = new FormData()
 			formData.append('email', email)
+			formData.append('rank', newRank)
+
+			await changeRank(formData)
+		} else {
+			console.log('All fields including email and rank are required!')
 		}
 	}
 
 	return (
-		<form>
-			<div className={styles.container}>
-				<div className={`${styles.container__content} flex flex-col items-center justify-center`}>
-					<p className="mb-8 text-center  font-neoregular text-primary-light">
-						Напиши email пользователя, чьё звание вы хотите поменять, а также укажите новое звание
-						для него.
-					</p>
-					<form className="flex flex-col items-center">
-						<Input
-							type="text"
-							name="email"
-							placeholder="Почта пользователя"
-							inputType="default"
-							required
-							onChange={(e) => setEmail(e.value)}
-						/>
-						<Input
-							type="text"
-							name="rank"
-							placeholder="Введите новое звание пользователя"
-							inputType="default"
-							margin="mt-4"
-							required
-						/>
-						<Button text="Поменять звание" buttonType="filled" margin="mt-8" />
-					</form>
-				</div>
+		<div className={styles.container}>
+			<div className={`${styles.container__content} flex flex-col items-center justify-center`}>
+				<p className="mb-8 text-center font-neoregular text-primary-light">
+					Напиши email пользователя, чьё звание вы хотите поменять, а также укажите новое звание для
+					него.
+				</p>
+				<form className="flex flex-col items-center" onSubmit={handleSubmit}>
+					<Input
+						type="text"
+						name="email"
+						placeholder="Почта пользователя"
+						inputType="default"
+						required
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<Input
+						type="text"
+						name="rank"
+						placeholder="Новое звание"
+						inputType="default"
+						margin="mt-4"
+						required
+						onChange={(e) => setRank(e.target.value)}
+					/>
+					<Button type="submit" text="Сменить ранг" buttonType="filled" margin="mt-4" />
+				</form>
 			</div>
-		</form>
+		</div>
 	)
 }
 
