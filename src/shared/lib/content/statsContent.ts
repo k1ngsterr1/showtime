c14ecdb084e0d97e98a25a12183e2e4f4ad04b42
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-04-20 15:48:40 +0600

    --UPDATED VERIFICATION

--- src/shared/lib/content/statsContent.ts
+++ src/shared/lib/content/statsContent.ts
@@ -2,19 +2,15 @@ const statsConfig = [
 	{
 		title: 'Тотал',
 		value: '100'
-		// any other properties you want to pass
 	},
 	{
 		title: 'Победы',
 		value: '70'
-		// any other properties you want to pass
 	},
 	{
 		title: 'Проигрыши',
 		value: '30'
-		// any other properties you want to pass
 	}
-	// ...more stats configuration
 ]
 
 export default statsConfig
